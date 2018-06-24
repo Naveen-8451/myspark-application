ba1cccc10047dc9a2db3ee125fe695bb0fa60347
Naveen0217 <divyakasireddy@Divyas-MBP.fios-router.home>
2018-06-23 20:57:55 -0400

    naveen

--- src/shared/transformation.py
+++ src/shared/transformation.py
@@ -3,6 +3,8 @@ from src.utils.read_from_db import (
     file_exits
 )
 from src.shared.exceptions import FileNotFoundException
+from pyspark.sql import functions as F
+import datetime
 import os
 
 
@@ -17,7 +19,15 @@ def create_transformed_file(spark, config, fs, incoming_file):
         raise FileNotFoundException(message)
 
     source_df = read_in_dataframe(spark, source_file, header=True)
-    source_df.write.partitionBy("zip").format("parquet").mode("overwrite").save(stg_target_file)
+    # TO Rename existing column Name use below statement.
+    source_df = source_df.withColumnRenamed("lat", "latitude")
+    source_df = source_df.withColumn("TimeStamp", F.lit(F.current_timestamp()))
+    source_df = source_df.withColumn("derive",F.concat("zip",F.lit("-"),"twp"))
+    source_df = source_df.filter("latitude='40.2432578'")
+    source_df = source_df.groupby("latitude").agg(F.max("latitude")).select("*")
+    source_df.show()
+
+    #source_df.write.partitionBy("zip").format("parquet").mode("overwrite").save(stg_target_file)
 
 
 
